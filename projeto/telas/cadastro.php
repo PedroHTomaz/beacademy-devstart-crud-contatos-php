176bec7151b4bde3268110df08d365053e8e24d5
Pedro Tomaz <pedrohtomaz2021@gmail.com>
2022-06-04 15:50:03 -0300

    Add in repository

---
+++ projeto/telas/cadastro.php
@@ -0,0 +1,8 @@
+<h1>Novo Contato</h1>
+<hr>
+<form action="" method="POST">
+    <input name="nome" class="form-control mb-3" type="text" placeholder="Digite seu nome...">
+    <input name="email" class="form-control mb-3" type="text" placeholder="Digite seu e-mail...">
+    <input name="telefone" class="form-control mb-3" type="text" placeholder="Digite seu telefone...">
+    <button class="btn btn-primary">Enviar</button>
+</form>
---
+++ projeto/telas/editar.php
@@ -0,0 +1,8 @@
+<h1>Editar Contato</h1>
+<hr>
+<form action="" method="POST">
+    <input value="<?php echo $dados[0]; ?>" name="nome" class="form-control mb-3" type="text" placeholder="Digite seu nome...">
+    <input value="<?php echo $dados[1]; ?>" name="email" class="form-control mb-3" type="text" placeholder="Digite seu e-mail...">
+    <input value="<?php echo $dados[2]; ?>" name="telefone" class="form-control mb-3" type="text" placeholder="Digite seu telefone...">
+    <button class="btn btn-primary">Enviar</button>
+</form>
